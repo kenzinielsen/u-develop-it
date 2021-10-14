7532f93d5a349b24bffcb2a58a0317268542ab99
Kenzi Nielsen <86693696+kenzinielsen@users.noreply.github.com>
2021-10-13 20:32:43 -0600

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection({
     // Your MySQL username,
     user: 'root',
     // Your MySQL password
-    password: 'Coko1414.',
+    password: '',
     database: 'election'
   });
 
